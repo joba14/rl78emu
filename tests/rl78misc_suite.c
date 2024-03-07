98e1792b273e17ad174d82a0cb3f0259c6164dd3
joba14 <57039383+4bstr4ct@users.noreply.github.com>
2024-03-07 21:05:53 +0200

    added misc module unit tests - passing

--- tests/rl78misc_suite.c
+++ tests/rl78misc_suite.c
@@ -16,19 +16,178 @@
 
 #include "./utester.h"
 
-utester_define_test(test1)
+/**
+ * @brief Construct a new utester define test for rl78misc_malloc.
+ */
+utester_define_test(rl78misc_malloc_test)
 {
-	utester_assert_true(1 == 1);
+	const void* const pointer = rl78misc_malloc(10);
+	utester_assert_not_equal(pointer, NULL);
+	rl78misc_free(pointer);
 }
 
-utester_define_test(test2)
+/**
+ * @brief Construct a new utester define test for rl78misc_realloc.
+ */
+utester_define_test(rl78misc_realloc_test)
 {
-	utester_assert_true(1 != 2);
+	const void* const pointer = rl78misc_realloc(NULL, 10);
+	utester_assert_not_equal(pointer, NULL);
+	rl78misc_free(pointer);
 }
 
-utester_define_test(test3)
+/**
+ * @brief Construct a new utester define test for rl78misc_free.
+ */
+utester_define_test(rl78misc_free_test)
 {
-	utester_assert_true(2 == 2);
+	const void* pointer = rl78misc_malloc(10);
+	utester_assert_not_equal(pointer, NULL);
+	pointer = rl78misc_free(pointer);
+	utester_assert_equal(pointer, NULL);
 }
 
-utester_run_suite(rl78misc_suite, &test1, &test2, &test3);
+/**
+ * @brief Construct a new utester define test for rl78misc_memset.
+ */
+utester_define_test(rl78misc_memset_test)
+{
+	uint8_t buffer[3] = {0};
+	utester_assert_equal(buffer[0], 0);
+	utester_assert_equal(buffer[1], 0);
+	utester_assert_equal(buffer[2], 0);
+
+	rl78misc_memset(buffer, 3, 3);
+	utester_assert_equal(buffer[0], 3);
+	utester_assert_equal(buffer[1], 3);
+	utester_assert_equal(buffer[2], 3);
+}
+
+/**
+ * @brief Construct a new utester define test for rl78misc_memcpy.
+ */
+utester_define_test(rl78misc_memcpy_test)
+{
+	uint8_t destination[3] = {0};
+	utester_assert_equal(destination[0], 0);
+	utester_assert_equal(destination[1], 0);
+	utester_assert_equal(destination[2], 0);
+
+	const uint8_t source[3] = {10, 20, 30};
+	rl78misc_memcpy(destination, source, 3);
+	utester_assert_equal(destination[0], 10);
+	utester_assert_equal(destination[1], 20);
+	utester_assert_equal(destination[2], 30);
+}
+
+/**
+ * @brief Construct a new utester define test for rl78misc_memcmp.
+ */
+utester_define_test(rl78misc_memcmp_test)
+{
+	{
+		const uint8_t buffer1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		const uint8_t buffer2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		const int32_t result = rl78misc_memcmp(buffer1, buffer2, 10);
+		utester_assert_equal(result, 0);
+	}
+
+	{
+		const uint8_t buffer1[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+		const uint8_t buffer2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		const int32_t result = rl78misc_memcmp(buffer1, buffer2, 10);
+		utester_assert_not_equal(result, 0);
+	}
+}
+
+/**
+ * @brief Construct a new utester define test for rl78misc_strlen.
+ */
+utester_define_test(rl78misc_strlen_test)
+{
+	{
+		const char_t buffer[] = "hello";
+		const uint64_t length = rl78misc_strlen(buffer);
+		utester_assert_equal(length, 5);
+	}
+
+	{
+		const char_t buffer[] = "he\0llo";
+		const uint64_t length = rl78misc_strlen(buffer);
+		utester_assert_equal(length, 2);
+	}
+}
+
+/**
+ * @brief Construct a new utester define test for rl78misc_strcmp.
+ */
+utester_define_test(rl78misc_strcmp_test)
+{
+	{
+		const char_t buffer1[] = "hello";
+		const char_t buffer2[] = "hello";
+		const int32_t result = rl78misc_strcmp(buffer1, buffer2);
+		utester_assert_equal(result, 0);
+	}
+
+	{
+		const char_t buffer1[] = "hello";
+		const char_t buffer2[] = "world";
+		const int32_t result = rl78misc_strcmp(buffer1, buffer2);
+		utester_assert_not_equal(result, 0);
+	}
+
+	{
+		const char_t buffer1[] = "hello";
+		const char_t buffer2[] = "hellooo";
+		const int32_t result = rl78misc_strcmp(buffer1, buffer2);
+		utester_assert_not_equal(result, 0);
+	}
+}
+
+/**
+ * @brief Construct a new utester define test for rl78misc_strncmp.
+ */
+utester_define_test(rl78misc_strncmp_test)
+{
+	{
+		const char_t buffer1[] = "hello";
+		const char_t buffer2[] = "hello";
+		const int32_t result = rl78misc_strncmp(buffer1, buffer2, 5);
+		utester_assert_equal(result, 0);
+	}
+
+	{
+		const char_t buffer1[] = "hello";
+		const char_t buffer2[] = "hello";
+		const int32_t result = rl78misc_strncmp(buffer1, buffer2, 3);
+		utester_assert_equal(result, 0);
+	}
+
+	{
+		const char_t buffer1[] = "hello";
+		const char_t buffer2[] = "world";
+		const int32_t result = rl78misc_strncmp(buffer1, buffer2, 3);
+		utester_assert_not_equal(result, 0);
+	}
+
+	{
+		const char_t buffer1[] = "hello";
+		const char_t buffer2[] = "hellooo";
+		const int32_t result = rl78misc_strncmp(buffer1, buffer2, 7);
+		utester_assert_not_equal(result, 0);
+	}
+}
+
+utester_run_suite(
+	rl78misc_suite,
+		&rl78misc_malloc_test,
+		&rl78misc_realloc_test,
+		&rl78misc_free_test,
+		&rl78misc_memset_test,
+		&rl78misc_memcpy_test,
+		&rl78misc_memcmp_test,
+		&rl78misc_strlen_test,
+		&rl78misc_strcmp_test,
+		&rl78misc_strncmp_test
+);
