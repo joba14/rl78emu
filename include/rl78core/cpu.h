882ea2c3e84b0ef7d90ae0476e57bd25f3782ccd
joba14 <57039383+joba14@users.noreply.github.com>
2024-03-09 18:29:34 +0000

    exposed gprs in the cpu public api and implemented their write and read functions

--- include/rl78core/cpu.h
+++ include/rl78core/cpu.h
@@ -15,10 +15,74 @@
 
 #include "rl78misc/common.h"
 
+#define rl78core_gpr08_x 0x00
+#define rl78core_gpr08_a 0x01
+#define rl78core_gpr08_c 0x02
+#define rl78core_gpr08_b 0x03
+#define rl78core_gpr08_e 0x04
+#define rl78core_gpr08_d 0x05
+#define rl78core_gpr08_l 0x06
+#define rl78core_gpr08_h 0x07
+#define rl78core_gpr08s_count 0x08
+
+#define rl78core_gpr16_ax 0x00
+#define rl78core_gpr16_bc 0x02
+#define rl78core_gpr16_de 0x04
+#define rl78core_gpr16_hl 0x06
+#define rl78core_gpr16s_count 0x08
+
+// todo: define all the sfrs here as offsets in their respective addressing ranges and functions to read and write.
+
 /**
  * @brief Initialize the cpu.
  */
 void rl78core_cpu_init(void);
+
+/**
+ * @brief Read the 10-bit value of the pc register.
+ * 
+ * @param value value to write to the pc register
+ */
+uint20_t rl78core_cpu_read_pc(void);
+
+/**
+ * @brief Read the 10-bit value of the pc register.
+ */
+void rl78core_cpu_write_pc(const uint20_t value);
+
+/**
+ * @brief Read the 8-bit value of provided general purpose register.
+ * 
+ * @param gpr08 offset of a 8-bit general purpose register in a bank
+ * 
+ * @return uint8_t value of the provided register
+ */
+uint8_t rl78core_cpu_read_gpr08(const uint8_t gpr08);
+
+/**
+ * @brief Write the 8-bit value to provided general purpose register.
+ * 
+ * @param gpr08 offset of a 8-bit general purpose register in a bank
+ * @param value value to write to the provided register
+ */
+void rl78core_cpu_write_gpr08(const uint8_t gpr08, const uint8_t value);
+
+/**
+ * @brief Read the 16-bit value of provided general purpose register.
+ * 
+ * @param gpr16 offset of a 16-bit general purpose register in a bank
+ * 
+ * @return uint16_t value of the provided register
+ */
+uint16_t rl78core_cpu_read_gpr16(const uint8_t gpr16);
+
+/**
+ * @brief Write the 16-bit value of provided general purpose register.
+ * 
+ * @param gpr16 offset of a 16-bit general purpose register in a bank
+ * @param value value to write to the provided register
+ */
+void rl78core_cpu_write_gpr16(const uint8_t gpr16, const uint16_t value);
 
 /**
  * @brief Halt the cpu.
--- source/rl78core/cpu.c
+++ source/rl78core/cpu.c
@@ -21,22 +21,6 @@
  * https://llvm-gcc-renesas.com/pdf/r01us0015ej0220_rl78.pdf @ 92 page.
  */
 
-#define rl78core_gpr_offset_x  0x00
-#define rl78core_gpr_offset_a  0x01
-#define rl78core_gpr_offset_ax 0x00
-	
-#define rl78core_gpr_offset_c  0x02
-#define rl78core_gpr_offset_b  0x03
-#define rl78core_gpr_offset_bc 0x02
-
-#define rl78core_gpr_offset_e  0x04
-#define rl78core_gpr_offset_d  0x05
-#define rl78core_gpr_offset_de 0x04
-
-#define rl78core_gpr_offset_l  0x06
-#define rl78core_gpr_offset_h  0x07
-#define rl78core_gpr_offset_hl 0x06
-
 typedef enum
 {
 	rl78core_fixed_sfr_spl = 0xFFFF8,  // note: the least significant bit must always be cleared to 0.
@@ -126,6 +110,48 @@ void rl78core_cpu_init(void)
 	};
 }
 
+uint20_t rl78core_cpu_read_pc(void)
+{
+	return g_rl78core_cpu.pc;
+}
+
+void rl78core_cpu_write_pc(const uint20_t value)
+{
+	g_rl78core_cpu.pc = value;
+}
+
+uint8_t rl78core_cpu_read_gpr08(const uint8_t gpr08)
+{
+	rl78misc_debug_assert(gpr08 < rl78core_gpr08s_count);
+	const uint20_t address = general_purpose_register_to_absolute_address(gpr08);
+	return rl78core_mem_read_u08(address);
+}
+
+void rl78core_cpu_write_gpr08(const uint8_t gpr08, const uint8_t value)
+{
+	rl78misc_debug_assert(gpr08 < rl78core_gpr08s_count);
+	const uint20_t address = general_purpose_register_to_absolute_address(gpr08);
+	rl78core_mem_write_u08(address, value);
+	// todo: handle flags if needed!
+}
+
+uint16_t rl78core_cpu_read_gpr16(const uint8_t gpr16)
+{
+	rl78misc_debug_assert(0 == (gpr16 % 2));
+	rl78misc_debug_assert(gpr16 < rl78core_gpr16s_count);
+	const uint20_t address = general_purpose_register_to_absolute_address(gpr16);
+	return rl78core_mem_read_u16(address);
+}
+
+void rl78core_cpu_write_gpr16(const uint8_t gpr16, const uint16_t value)
+{
+	rl78misc_debug_assert(0 == (gpr16 % 2));
+	rl78misc_debug_assert(gpr16 < rl78core_gpr16s_count);
+	const uint20_t address = general_purpose_register_to_absolute_address(gpr16);
+	rl78core_mem_write_u16(address, value);
+	// todo: handle flags if needed!
+}
+
 void rl78core_cpu_halt(void)
 {
 	g_rl78core_cpu.halted = true;
@@ -148,67 +174,52 @@ void rl78core_cpu_tick(void)
 		case 0x50:  // MOV X, #byte
 		{
 			const uint8_t data = fetch_instruction_byte();
-			const uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_x);
-			rl78core_mem_write_u08(absolute_address, data);
-			// todo: handle flags if needed!
+			rl78core_cpu_write_gpr08(rl78core_gpr08_x, data);
 		} break;
 
 		case 0x51:  // MOV A, #byte
 		{
 			const uint8_t data = fetch_instruction_byte();
-			const uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
-			rl78core_mem_write_u08(absolute_address, data);
-			// todo: handle flags if needed!
+			rl78core_cpu_write_gpr08(rl78core_gpr08_a, data);
 		} break;
 
 		case 0x52:  // MOV C, #byte
 		{
 			const uint8_t data = fetch_instruction_byte();
-			const uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_c);
-			rl78core_mem_write_u08(absolute_address, data);
-			// todo: handle flags if needed!
+			rl78core_cpu_write_gpr08(rl78core_gpr08_c, data);
 		} break;
 
 		case 0x53:  // MOV B, #byte
 		{
 			const uint8_t data = fetch_instruction_byte();
-			const uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_b);
-			rl78core_mem_write_u08(absolute_address, data);
-			// todo: handle flags if needed!
+			rl78core_cpu_write_gpr08(rl78core_gpr08_b, data);
 		} break;
 
 		case 0x54:  // MOV E, #byte
 		{
 			const uint8_t data = fetch_instruction_byte();
-			const uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_e);
-			rl78core_mem_write_u08(absolute_address, data);
-			// todo: handle flags if needed!
+			rl78core_cpu_write_gpr08(rl78core_gpr08_e, data);
 		} break;
 
 		case 0x55:  // MOV D, #byte
 		{
 			const uint8_t data = fetch_instruction_byte();
-			const uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_d);
-			rl78core_mem_write_u08(absolute_address, data);
-			// todo: handle flags if needed!
+			rl78core_cpu_write_gpr08(rl78core_gpr08_d, data);
 		} break;
 
 		case 0x56:  // MOV L, #byte
 		{
 			const uint8_t data = fetch_instruction_byte();
-			const uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_l);
-			rl78core_mem_write_u08(absolute_address, data);
-			// todo: handle flags if needed!
+			rl78core_cpu_write_gpr08(rl78core_gpr08_l, data);
 		} break;
 
 		case 0x57:  // MOV H, #byte
 		{
 			const uint8_t data = fetch_instruction_byte();
-			const uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_h);
-			rl78core_mem_write_u08(absolute_address, data);
-			// todo: handle flags if needed!
+			rl78core_cpu_write_gpr08(rl78core_gpr08_h, data);
 		} break;
 
+#if 0
 		case 0xCD:  // MOV saddr, #byte
 		{
 			const uint8_t saddr = fetch_instruction_byte();
@@ -239,8 +250,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x60:  // MOV A, X
 		{
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
-			const uint20_t absolute_address_x = general_purpose_register_to_absolute_address(rl78core_gpr_offset_x);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
+			const uint20_t absolute_address_x = general_purpose_register_to_absolute_address(rl78core_gpr08_x);
 			const uint8_t x_value = rl78core_mem_read_u08(absolute_address_x);
 			rl78core_mem_write_u08(absolute_address_a, x_value);
 			// todo: handle flags if needed!
@@ -248,8 +259,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x62:  // MOV A, C
 		{
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
-			const uint20_t absolute_address_c = general_purpose_register_to_absolute_address(rl78core_gpr_offset_c);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
+			const uint20_t absolute_address_c = general_purpose_register_to_absolute_address(rl78core_gpr08_c);
 			const uint8_t c_value = rl78core_mem_read_u08(absolute_address_c);
 			rl78core_mem_write_u08(absolute_address_a, c_value);
 			// todo: handle flags if needed!
@@ -257,8 +268,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x63:  // MOV A, B
 		{
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
-			const uint20_t absolute_address_b = general_purpose_register_to_absolute_address(rl78core_gpr_offset_b);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
+			const uint20_t absolute_address_b = general_purpose_register_to_absolute_address(rl78core_gpr08_b);
 			const uint8_t b_value = rl78core_mem_read_u08(absolute_address_b);
 			rl78core_mem_write_u08(absolute_address_a, b_value);
 			// todo: handle flags if needed!
@@ -266,8 +277,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x64:  // MOV A, E
 		{
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
-			const uint20_t absolute_address_e = general_purpose_register_to_absolute_address(rl78core_gpr_offset_e);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
+			const uint20_t absolute_address_e = general_purpose_register_to_absolute_address(rl78core_gpr08_e);
 			const uint8_t e_value = rl78core_mem_read_u08(absolute_address_e);
 			rl78core_mem_write_u08(absolute_address_a, e_value);
 			// todo: handle flags if needed!
@@ -275,8 +286,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x65:  // MOV A, D
 		{
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
-			const uint20_t absolute_address_d = general_purpose_register_to_absolute_address(rl78core_gpr_offset_d);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
+			const uint20_t absolute_address_d = general_purpose_register_to_absolute_address(rl78core_gpr08_d);
 			const uint8_t d_value = rl78core_mem_read_u08(absolute_address_d);
 			rl78core_mem_write_u08(absolute_address_a, d_value);
 			// todo: handle flags if needed!
@@ -284,8 +295,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x66:  // MOV A, L
 		{
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
-			const uint20_t absolute_address_l = general_purpose_register_to_absolute_address(rl78core_gpr_offset_l);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
+			const uint20_t absolute_address_l = general_purpose_register_to_absolute_address(rl78core_gpr08_l);
 			const uint8_t l_value = rl78core_mem_read_u08(absolute_address_l);
 			rl78core_mem_write_u08(absolute_address_a, l_value);
 			// todo: handle flags if needed!
@@ -293,8 +304,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x67:  // MOV A, H
 		{
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
-			const uint20_t absolute_address_h = general_purpose_register_to_absolute_address(rl78core_gpr_offset_h);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
+			const uint20_t absolute_address_h = general_purpose_register_to_absolute_address(rl78core_gpr08_h);
 			const uint8_t h_value = rl78core_mem_read_u08(absolute_address_h);
 			rl78core_mem_write_u08(absolute_address_a, h_value);
 			// todo: handle flags if needed!
@@ -302,8 +313,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x70:  // MOV X, A
 		{
-			const uint20_t absolute_address_x = general_purpose_register_to_absolute_address(rl78core_gpr_offset_x);
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			const uint20_t absolute_address_x = general_purpose_register_to_absolute_address(rl78core_gpr08_x);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			const uint8_t a_value = rl78core_mem_read_u08(absolute_address_a);
 			rl78core_mem_write_u08(absolute_address_x, a_value);
 			// todo: handle flags if needed!
@@ -311,8 +322,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x72:  // MOV C, A
 		{
-			const uint20_t absolute_address_c = general_purpose_register_to_absolute_address(rl78core_gpr_offset_c);
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			const uint20_t absolute_address_c = general_purpose_register_to_absolute_address(rl78core_gpr08_c);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			const uint8_t a_value = rl78core_mem_read_u08(absolute_address_a);
 			rl78core_mem_write_u08(absolute_address_c, a_value);
 			// todo: handle flags if needed!
@@ -320,8 +331,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x73:  // MOV B, A
 		{
-			const uint20_t absolute_address_b = general_purpose_register_to_absolute_address(rl78core_gpr_offset_b);
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			const uint20_t absolute_address_b = general_purpose_register_to_absolute_address(rl78core_gpr08_b);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			const uint8_t a_value = rl78core_mem_read_u08(absolute_address_a);
 			rl78core_mem_write_u08(absolute_address_b, a_value);
 			// todo: handle flags if needed!
@@ -329,8 +340,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x74:  // MOV E, A
 		{
-			const uint20_t absolute_address_e = general_purpose_register_to_absolute_address(rl78core_gpr_offset_e);
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			const uint20_t absolute_address_e = general_purpose_register_to_absolute_address(rl78core_gpr08_e);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			const uint8_t a_value = rl78core_mem_read_u08(absolute_address_a);
 			rl78core_mem_write_u08(absolute_address_e, a_value);
 			// todo: handle flags if needed!
@@ -338,8 +349,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x75:  // MOV D, A
 		{
-			const uint20_t absolute_address_d = general_purpose_register_to_absolute_address(rl78core_gpr_offset_d);
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			const uint20_t absolute_address_d = general_purpose_register_to_absolute_address(rl78core_gpr08_d);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			const uint8_t a_value = rl78core_mem_read_u08(absolute_address_a);
 			rl78core_mem_write_u08(absolute_address_d, a_value);
 			// todo: handle flags if needed!
@@ -347,8 +358,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x76:  // MOV L, A
 		{
-			const uint20_t absolute_address_l = general_purpose_register_to_absolute_address(rl78core_gpr_offset_l);
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			const uint20_t absolute_address_l = general_purpose_register_to_absolute_address(rl78core_gpr08_l);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			const uint8_t a_value = rl78core_mem_read_u08(absolute_address_a);
 			rl78core_mem_write_u08(absolute_address_l, a_value);
 			// todo: handle flags if needed!
@@ -356,8 +367,8 @@ void rl78core_cpu_tick(void)
 
 		case 0x77:  // MOV H, A
 		{
-			const uint20_t absolute_address_h = general_purpose_register_to_absolute_address(rl78core_gpr_offset_h);
-			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			const uint20_t absolute_address_h = general_purpose_register_to_absolute_address(rl78core_gpr08_h);
+			const uint20_t absolute_address_a = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			const uint8_t a_value = rl78core_mem_read_u08(absolute_address_a);
 			rl78core_mem_write_u08(absolute_address_h, a_value);
 			// todo: handle flags if needed!
@@ -368,22 +379,30 @@ void rl78core_cpu_tick(void)
 			const uint8_t short_address = fetch_instruction_byte();
 			uint20_t absolute_address = short_direct_address_to_absolute_address(short_address);
 			const uint8_t data = rl78core_mem_read_u08(absolute_address);
-			absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			rl78core_mem_write_u08(absolute_address, data);
 			// todo: handle flags if needed!
 		} break;
 
 		case 0x9D:  // MOV saddr, A
 		{
-			uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr_offset_a);
+			uint20_t absolute_address = general_purpose_register_to_absolute_address(rl78core_gpr08_a);
 			const uint8_t data = rl78core_mem_read_u08(absolute_address);
 			const uint8_t short_address = fetch_instruction_byte();
 			absolute_address = short_direct_address_to_absolute_address(short_address);
 			rl78core_mem_write_u08(absolute_address, data);
 			// todo: handle flags if needed!
 		} break;
+#endif
 
 		// -------------------------------------------------------- //
+
+		// -------------------------------------------------------- //
+
+		default:
+		{
+			g_rl78core_cpu.halted = true;
+		} break;
 	}
 }
 
