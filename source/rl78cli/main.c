2adb2ef9ba1bde74fb9cefaf188368a5e27f8200
joba14 <57039383+4bstr4ct@users.noreply.github.com>
2024-03-10 21:34:29 +0200

    added a test case "program" in main and tested it + cpu stats logging

--- source/rl78cli/main.c
+++ source/rl78cli/main.c
@@ -19,6 +19,10 @@
 #include "rl78cli/config.h"
 #include "rl78cli/main.h"
 
+#include <stdio.h>
+
+static void display_cpu_state(const uint64_t tick);
+
 static void flash_rom_to_mem(const uint8_t* const rom, const uint20_t length);
 
 int32_t main(const int32_t argc, const char_t* argv[])
@@ -32,18 +36,59 @@ int32_t main(const int32_t argc, const char_t* argv[])
 	rl78core_cpu_init();
 
 	// todo: flash the binary into the mem:
-	#define rom_length 2
-	const uint8_t rom[rom_length] = { 0x50, 0x69 };
+	// const uint8_t rom[] = { 0x50, 0x69, 0xCE, 0xFA, 0x0A, 0x00, 0x51, 0x05, 0x00, 0x00, 0x01 };
+	const uint8_t rom[] =
+	{
+		0x50, 0x69,
+		0x51, 0x5,
+		0xCE, 0xFA, 0x03,
+		0x50, 0x69,
+		0x51, 0x5,
+		0x01
+	};
+	#define rom_length (sizeof(rom) / sizeof(rom[0]))
 	flash_rom_to_mem(rom, rom_length);
 
 	for (uint64_t tick_count = 0; !rl78core_cpu_halted(); ++tick_count)
 	{
+		rl78misc_logger_log("old state:");
 		rl78core_cpu_tick();
-		rl78misc_logger_log("tick_count=%lu", tick_count);
-		rl78misc_logger_log("----------");
+		rl78misc_logger_log("new state:");
+		display_cpu_state(tick_count);
+		getchar();
 	}
 
 	return 0;
+}
+
+static void display_cpu_state(const uint64_t tick)
+{
+	rl78core_cpu_stats_s stats;
+	rl78core_cpu_get_stats(&stats);
+
+	rl78misc_logger_log("cpu state:");
+	rl78misc_logger_log("    tick= %lu", tick);
+	rl78misc_logger_log("    pc  = 0x%08X (%u)", stats.pc,  stats.pc );
+	rl78misc_logger_log("    x   = 0x%08X (%u)", stats.x,   stats.x  );
+	rl78misc_logger_log("    a   = 0x%08X (%u)", stats.a,   stats.a  );
+	rl78misc_logger_log("    c   = 0x%08X (%u)", stats.c,   stats.c  );
+	rl78misc_logger_log("    b   = 0x%08X (%u)", stats.b,   stats.b  );
+	rl78misc_logger_log("    e   = 0x%08X (%u)", stats.e,   stats.e  );
+	rl78misc_logger_log("    d   = 0x%08X (%u)", stats.d,   stats.d  );
+	rl78misc_logger_log("    l   = 0x%08X (%u)", stats.l,   stats.l  );
+	rl78misc_logger_log("    h   = 0x%08X (%u)", stats.h,   stats.h  );
+	rl78misc_logger_log("    ax  = 0x%08X (%u)", stats.ax,  stats.ax );
+	rl78misc_logger_log("    bc  = 0x%08X (%u)", stats.bc,  stats.bc );
+	rl78misc_logger_log("    de  = 0x%08X (%u)", stats.de,  stats.de );
+	rl78misc_logger_log("    hl  = 0x%08X (%u)", stats.hl,  stats.hl );
+	rl78misc_logger_log("    spl = 0x%08X (%u)", stats.spl, stats.spl);
+	rl78misc_logger_log("    sph = 0x%08X (%u)", stats.sph, stats.sph);
+	rl78misc_logger_log("    psw = 0x%08X (%u)", stats.psw, stats.psw);
+	rl78misc_logger_log("    cs  = 0x%08X (%u)", stats.cs,  stats.cs );
+	rl78misc_logger_log("    es  = 0x%08X (%u)", stats.es,  stats.es );
+	rl78misc_logger_log("    pmc = 0x%08X (%u)", stats.pmc, stats.pmc);
+	rl78misc_logger_log("    mem = 0x%08X (%u)", stats.mem, stats.mem);
+	rl78misc_logger_log("---------");
 }
 
 static void flash_rom_to_mem(const uint8_t* const rom, const uint20_t length)
