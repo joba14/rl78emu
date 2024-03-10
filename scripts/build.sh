1ee751112accfe4e8ef30db63853f9ab8f847ec6
joba14 <57039383+4bstr4ct@users.noreply.github.com>
2024-03-10 21:33:11 +0200

    updated build.sh and rebuild.sh scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,29 +7,6 @@ cd $PROJECT_DIR
 
 # --------------------------------------------------------------------------- #
 
-# Set the build configuration (default to debug if not provided)
-BUILD_CONFIGURATION=${1:-"debug"}
-
-# Choose compiler flags and defines based on build configuration
-case $BUILD_CONFIGURATION in
-	"debug")
-		FLAGS="-g -O0"
-		DEFINES=""
-		;;
-	"hybrid")
-		FLAGS="-g -O2"
-		DEFINES=""
-		;;
-	"release")
-		FLAGS="-O3"
-		DEFINES="-DNDEBUG"
-		;;
-	*)
-		echo "[error]: invalid build configuration. Please use 'debug', 'hybrid', or 'release'."
-		exit 1
-		;;
-esac
-
 # Build command
 cd ./build
 make
--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,13 +1,38 @@
 
 # !/bin/sh
 
+# Set the build configuration (default to debug if not provided)
+BUILD_CONFIGURATION=${1:-"debug"}
+
+# Choose compiler flags and defines based on build configuration
+case $BUILD_CONFIGURATION in
+	"debug")
+		FLAGS="-g -O0"
+		DEFINES=""
+		;;
+	"hybrid")
+		FLAGS="-g -O2"
+		DEFINES=""
+		;;
+	"release")
+		FLAGS="-O3"
+		DEFINES="-DNDEBUG"
+		;;
+	*)
+		echo "[error]: invalid build configuration. Please use 'debug', 'hybrid', or 'release'."
+		exit 1
+		;;
+esac
+
 ./clean.sh
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PROJECT_DIR="$SCRIPT_DIR/.."
 cd $PROJECT_DIR
-mkdir "./build"
+
 ./autogen.sh
+mkdir "./build"
+
 cd $PROJECT_DIR/build
 ./../configure CFLAGS="$FLAGS" CPPFLAGS="$DEFINES"
 cd $SCRIPT_DIR
