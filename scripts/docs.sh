89c2ebcd56be4f9acbc6e7ea930562a4b6955819
joba14 <57039383+4bstr4ct@users.noreply.github.com>
2024-03-10 23:03:41 +0200

    added docs.sh script to scripts dir

---
+++ scripts/docs.sh
@@ -0,0 +1,11 @@
+
+# !/bin/sh
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_DIR="$SCRIPT_DIR/.."
+cd $PROJECT_DIR
+
+# --------------------------------------------------------------------------- #
+
+# Docs command
+doxygen ./doxyfile.sh
